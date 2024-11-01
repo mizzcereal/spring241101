58ac7c6888531b6659ab09901f0698aebedc1270
mizzcereal <131233843+mizzcereal@users.noreply.github.com>
2024-11-01 18:55:15 +0900

    기본설정

---
+++ src/main/java/com/example/spring1101/Spring1101Application.java
@@ -0,0 +1,13 @@
+package com.example.spring1101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1101Application.class, args);
+	}
+
+}
