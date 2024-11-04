cf8092b01f917e3652acfc532a08d46c44e78f72
mizzcereal <131233843+mizzcereal@users.noreply.github.com>
2024-11-04 17:31:22 +0900

    기본 Controller 구성

---
+++ src/main/java/com/example/spring1101/Controller/MyController.java
@@ -0,0 +1,13 @@
+package com.example.spring1101.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home/home";
+    }
+}
